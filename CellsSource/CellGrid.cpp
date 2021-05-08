04ed0ca59f716db81b8a29562aad33ec9c740280
vlad-creator <Vlad-Loz@yandex.ru>
2021-05-09 01:48:15 +0300

    added thread

--- CellsSource/CellGrid.cpp
+++ CellsSource/CellGrid.cpp
@@ -4,8 +4,7 @@
 CellGrid::CellGrid() :
 	grid_(std::vector<std::vector<BasicCell*>>(GRID_SIZE_Y, std::vector<BasicCell*>(GRID_SIZE_X, nullptr))) {}
 
-
-void CellGrid::SpawnCell(BasicCell* cell) {
+void CellGrid::SpawnCell_thr1(BasicCell* cell) {
 	auto pos = cell->GetGridPos();
 	if (pos.y >= 0 && pos.y < GRID_SIZE_Y) {
 		if (cells_.find(cell) == cells_.end()) {
@@ -25,25 +24,121 @@ void CellGrid::SpawnCell(BasicCell* cell) {
 		delete cell;
 	}
 }
+
+std::mutex check_spawn;
+void CellGrid::SpawnCell(BasicCell* cell) {
+	check_spawn.lock();
+	auto pos = cell->GetGridPos();
+	if (pos.y >= 0 && pos.y < GRID_SIZE_Y) {
+		if (cells_.find(cell) == cells_.end()) {
+			cell->SetGrid(this);
+			cells_.insert(cell);
+			DeleteCell(grid_[pos.y][pos.x % GRID_SIZE_X]);
+			grid_[pos.y][pos.x % GRID_SIZE_X] = cell;
+		}
+		else {
+			std::cout << "Clone!!\n";
+			delete cell;
+		}
+	}
+	else {
+		std::cout << "Out of grid or at same pos!\n";
+		delete cell;
+	}
+	check_spawn.unlock();
+}
+
 void CellGrid::DeleteCell(Vector2I pos) {
 	auto cell = grid_[pos.y][pos.x];
 	DeleteCell(cell);
 }
 
 int aaa = 0;
+std::mutex check;
 void CellGrid::DeleteCell(BasicCell* cell) {
+	check.lock();
+	
 	if (cell != nullptr) {
 		cell->Delete();
 		cells_to_delete_.push_back(cell);
 		grid_[cell->GetGridPos().y][cell->GetGridPos().x] = nullptr;
 		aaa++;
 	}
+
+	check.unlock();
 }
 
-void CellGrid::UpdateCells() {
+/*void CellGrid::UpdateCells() {
 	for (auto& cell : cells_)
 		if (!cell->IsToDelete())
 			cell->Update();
+	ClearDeletedCells();
+}*/
+
+void CellGrid::UpdateCells_1()
+{
+	for (auto& cell : cells_)
+	{
+		if (((cell->GetGridPos()).x > GRID_SIZE_X / 2) && ((cell->GetGridPos()).y > GRID_SIZE_Y / 2) && ((cell->GetGridPos()).x < GRID_SIZE_X) && ((cell->GetGridPos()).y < GRID_SIZE_Y))
+			if (!cell->IsToDelete())
+				cell->Update();
+	}
+}
+
+void CellGrid::UpdateCells_2()
+{
+	for (auto& cell : cells_)
+	{
+		if (((cell->GetGridPos()).x > GRID_SIZE_X / 2) && ((cell->GetGridPos()).y < GRID_SIZE_Y / 2) && ((cell->GetGridPos()).x < GRID_SIZE_X) && ((cell->GetGridPos()).y > 0))
+			if (!cell->IsToDelete())
+				cell->Update();
+	}
+}
+
+void CellGrid::UpdateCells_3()
+{
+	for (auto& cell : cells_)
+	{
+		if (((cell->GetGridPos()).x < GRID_SIZE_X / 2) && ((cell->GetGridPos()).y < GRID_SIZE_Y / 2) && ((cell->GetGridPos()).x > 0) && ((cell->GetGridPos()).y > 0))
+			if (!cell->IsToDelete())
+				cell->Update();
+	}
+}
+
+void CellGrid::UpdateCells_4()
+{
+	for (auto& cell : cells_)
+	{
+		if (((cell->GetGridPos()).x < GRID_SIZE_X / 2) && ((cell->GetGridPos()).y > GRID_SIZE_Y / 2) && ((cell->GetGridPos()).x > 0) && ((cell->GetGridPos()).y < GRID_SIZE_Y))
+			if (!cell->IsToDelete())
+				cell->Update();
+	}
+}
+
+void CellGrid::UpdateCells_cross()
+{
+	for (auto& cell : cells_)
+	{
+		if (((cell->GetGridPos()).x == GRID_SIZE_X / 2) || ((cell->GetGridPos()).y == GRID_SIZE_Y / 2) || ((cell->GetGridPos()).x == 0) || ((cell->GetGridPos()).y == 0))
+			if (!cell->IsToDelete())
+				cell->Update();
+	}
+}
+
+
+void CellGrid::UpdateCells()
+{
+	std::thread thr_1(&CellGrid::UpdateCells_1 , this);
+	std::thread thr_2(&CellGrid::UpdateCells_2 , this);
+	std::thread thr_3(&CellGrid::UpdateCells_3 , this);
+	std::thread thr_4(&CellGrid::UpdateCells_4 , this);
+	thr_1.join();
+	thr_2.join();
+	thr_3.join();
+	thr_4.join();
+
+	UpdateCells_cross();
+
 	ClearDeletedCells();
 }
 
